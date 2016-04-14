8ddf5aff250d4b4f9607247af7c641b6c9d3d0ee
dmckinney5 <dymc7938@colorado.edu>
2016-04-14 13:13:37 -0600

    Changed QUITE A BIT. threadedserver is now multiprocess not multithread, and is a class (not that that is a big change). Both server's now send a valid http header, and the test.html. Client can be pretty much ignored, as httperf is now what we are using for testing

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import socket
 import sys
-
  
 HOST = '127.0.0.1'
 PORT = 8080 
@@ -18,28 +17,33 @@ except socket.error as msg:
 print('Socket Binded')
  
 #Start listening on socket
-s.listen(512)
+s.listen(20)
 print('Socket Listening')
  
 #wait for clients fo foeva
+
 while 1 < 2:
     #accept connection
     conn, addr = s.accept()
     print('Connected with ' + addr[0] + ':' + str(addr[1]))
 
     #handle request
-    data = conn.recv(1024)
-    test = data.decode().split()
+    data = conn.recv(4096)
+    #test = data.decode().split()
     #log the request
     with open('RequestLogNonCon.txt', 'a') as f:
-            f.write(test[0] +' '+ test[1] +" at: "+' '.join(map(str,addr))+ "\n")
+            f.write(data.decode() + " at: "+' '.join(map(str,addr))+ "\n")
             #f.write(' '.join(map(str,addr))+ "\n")
     #send confirmation, close connection   
-    sendOff = "Request Recorded from "+ ''.join(map(str,addr)) + " connection closing"
+    #sendOff = "Request Recorded from "+ ''.join(map(str,addr)) + " connection closing"
     #conn.send(sendOff.encode())
-    conn.send('HTTP/1.0 200 OK\n'.encode())
-    conn.send('Content-Type: text/html\n'.encode())
-    conn.send(sendOff.encode()) # header and body should be separated by additional newline
+    conn.send('HTTP/1.1 200 OK\nContent-Type: text/html\n\n'.encode())
+    file = open("test.html","rb")
+    while True:
+        toSend = file.read(65536)
+        if not toSend:
+            break  # EOF
+        conn.sendall(toSend)
     conn.close()
     
  
--- threadedserver.py
+++ threadedserver.py
@@ -1,36 +1,47 @@
 import socket
 import sys
-import threading
+import multiprocessing
 #implementation tutorial found on http://www.binarytides.com/
 #Designate localhost, and which port to listen on 
 HOST = '127.0.0.1'
 PORT = 8080 
 # we will use the lock to protect our file access
-lock = threading.Lock()
-#create socket 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket Created')
- 
-try:
-    #bind socket to host and port
-    s.bind((HOST, PORT))
+lock = multiprocessing.Lock()
 
-except socket.error as msg:
-    #commonly get port already in use error message
-    print(msg)
-    sys.exit()
-     
-print('Socket Binded')
- 
-#Listen on the socket
-s.listen(512)
-print('Socket Listening')
- 
+class Server(object):
+
+	def __init__(self,hostname,port):
+		self.hostname = hostname
+		self.port = port
+	def start(self):
+		self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		print('Socket Created')
+		s = self.socket.bind((self.hostname,self.port))
+		print('Socket Binded')
+		self.socket.listen(20)
+		print('Socket Listening')
+
+		while 1 < 2:
+			conn, addy = self.socket.accept()
+			print('Connected with ' + addy[0] + ':' + str(addy[1]))
+			process = multiprocessing.Process(target=client_handler, args=(conn,addy))
+			process.daemon = True
+			process.start()
+			conn.close()
+
+	def close(self):
+		s.shutdown()
+		s.close()
+
+
+
+
+
 
 #Handle incoming client requests
 def client_handler(conn,addy): 
     #Receiving from client
-    data = conn.recv(1024)
+    data = conn.recv(4096)
     #attempt to snag lock
     lock.acquire()
     try:
@@ -38,27 +49,29 @@ def client_handler(conn,addy):
         with open('RequestLog.txt', 'a') as f:
             #write our message, and the address it was received from
             f.write(data.decode() +" at: ")
-            f.write(' '.join(map(str,addy))+ "\n")
+            f.write(''.join(map(str,addy))+ "\n")
     
     finally:
         #release lock
-        lock.release()
-        #generate and send closing message. close connection
-        closing = "Request Recorded from "+ threading.currentThread().getName()+ " at "  + ''.join(map(str,addy)) + " connection closing"
-        conn.send('HTTP/1.0 200 OK\n'.encode())
-        conn.send('Content-Type: text/html\n'.encode())
-        conn.send(closing.encode())    
+        conn.send('HTTP/1.1 200 OK\nContent-Type: text/html\n\n'.encode())
+        file = open("test.html","rb")
+        while True:
+            toSend = file.read(65536)
+            if not toSend:
+                break  # EOF
+            conn.sendall(toSend)
         
-    conn.close()
- 
-#Listen infinitiely long for new clients, delegate new thread to handle client request. 
-while 1 < 2:
-	#accept new connection 
-    conn, addr = s.accept()
-    print('Connected with ' + addr[0] + ':' + str(addr[1]))
-     
-    #start new thread,pass function for thread to run, and the tuple of thread arguments
-    t = threading.Thread(target=client_handler,args=(conn,addr))
-    t.start()
- 
-s.close()
+        lock.release()
+        
+   
+    
+
+if __name__ == "__main__":
+    server = Server(HOST, PORT)
+    try:
+        server.start()
+    finally:
+        for process in multiprocessing.active_children():
+            process.terminate()
+            process.join()
+            server.close()
