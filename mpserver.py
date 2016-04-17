1a04747b0cb966f9e6f2282a774ef5a0610358c3
dmckinney5 <dymc7938@colorado.edu>
2016-04-17 14:13:20 -0600

    adding mpserver, due to referencing it in the write up

---
+++ mpserver.py
@@ -0,0 +1,77 @@
+import socket
+import sys
+import multiprocessing
+#implementation tutorial found on http://www.binarytides.com/
+#Designate localhost, and which port to listen on 
+HOST = '127.0.0.1'
+PORT = 8880
+# we will use the lock to protect our file access
+lock = multiprocessing.Lock()
+
+class Server(object):
+
+	def __init__(self,hostname,port):
+		self.hostname = hostname
+		self.port = port
+	def start(self):
+		self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		print('Socket Created')
+		s = self.socket.bind((self.hostname,self.port))
+		print('Socket Binded')
+		self.socket.listen(20)
+		print('Socket Listening')
+
+		while 1 < 2:
+			conn, addy = self.socket.accept()
+			conn.send('HTTP/1.1 200 OK\nContent-Type: text/html\n\n'.encode())
+            #print('Connected with ' + addy[0] + ':' + str(addy[1]))
+			process = multiprocessing.Process(target=client_handler, args=(conn,addy))
+			process.daemon = True
+			process.start()
+			conn.close()
+
+	def close(self):
+		s.shutdown()
+		s.close()
+
+
+
+
+
+
+#Handle incoming client requests
+def client_handler(conn,addy): 
+    #Receiving from client
+    data = conn.recv(4096)
+    #attempt to snag lock
+    lock.acquire()
+    try:
+        #if lock grabbed
+        with open('RequestLog.txt', 'a') as f:
+            #write our message, and the address it was received from
+            f.write(data.decode() +" at: ")
+            f.write(''.join(map(str,addy))+ "\n")
+    
+    finally:
+        #release lock
+        file = open("test.html","rb")
+        while True:
+            toSend = file.read(65536)
+            if not toSend:
+                break  # EOF
+            conn.sendall(toSend)
+        
+        lock.release()
+        
+   
+    
+
+if __name__ == "__main__":
+    server = Server(HOST, PORT)
+    try:
+        server.start()
+    finally:
+        for process in multiprocessing.active_children():
+            process.terminate()
+            process.join()
+            server.close()
